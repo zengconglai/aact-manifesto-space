e70bf25edcd7d17de6623ee54ccdf21e5f3ae551
zengconglai <zengcong.lai@gmail.com>
2025-05-08 10:18:51 -0400

    Updated the about us page and added Carol's bio

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -6,11 +6,20 @@ import 'vueperslides/dist/vueperslides.css'
 defineProps({ slides: Array })
 </script>
 <template>
-    <vueper-slides lazy lazy-load-on-drag class="no-shadow" :infinite="false" :bullets="false" :slide-ratio="99 / 100">
-        <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image" :content="slide.content" />
+    <vueper-slides lazy lazy-load-on-drag class="no-shadow" :infinite="false" :bullets="false">
+        <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image" :content="slide.content" class="carousel-slide" />
     </vueper-slides>
 </template>
 
 <style>
     .vueperslides__arrow {color: black;}
+    .vueperslides__slide {
+        height: auto !important;
+        min-height: 500px;
+    }
+    .vueperslides__slide img {
+        max-height: 80vh;
+        width: auto;
+        margin: 0 auto;
+    }
 </style>
